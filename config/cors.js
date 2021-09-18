cb8fb0d0571a723a784cf9c81e17f92f7184e375
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-18 14:36:04 +0200

    new deploy adds new env vars

--- config/cors.js
+++ config/cors.js
@@ -3,6 +3,6 @@ const corsOptions = {
     ? [process.env.CLIENT_URL, process.env.CLIENT_URL_BUILD]
     : [process.env.CLIENT_URL_BUILD],
   optionsSuccessStatus: 200,
-};
+}; //
 
 module.exports = { corsOptions };
